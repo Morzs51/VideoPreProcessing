fd1c510786c2c5a9f91778c32f03a09b5ffbdc52
Morzs51 <101831380+Morzs51@users.noreply.github.com>
2023-08-29 17:13:26 +0300

    config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 class Config:
     def __init__(self):
-        self.device = 0
-        self.path = ''
-        self.fps = 30
-        self.blur_threshold = 120
-        self.frame_interval = 5
+        self.device = 0                 # Номер используемой камеры
+        self.path = ''                  # Путь до видеофайла
+        self.fps = 30                   # Количество кадров в видео
+        self.blur_threshold = 120       # Порог размытости изображения
+        self.frame_interval = 5         # Интервал времени в который берется кадр 5 = 0.005
