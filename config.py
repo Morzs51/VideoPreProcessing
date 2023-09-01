7f4d6dab3c798d53256e24cf0f82f1ff657766de
Morzs51 <101831380+Morzs51@users.noreply.github.com>
2023-09-01 23:10:31 +0300

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-class Config:
-    def __init__(self):
-        self.device = 0                 # Номер используемой камеры
-        self.path = ''                  # Путь до видеофайла
-        self.fps = 30                   # Количество кадров в видео
-        self.blur_threshold = 120       # Порог размытости изображения
-        self.frame_interval = 5         # Интервал времени в который берется кадр 5 = 0.005
